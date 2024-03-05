b9ef571a3d2fca7b4f15b478968919475ddcd64c
sac1010 <sachingirish101@gmail.com>
2024-03-06 00:34:37 +0530

    verify email added

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignUpForm = ({ basicMode }) => {
   const [formData, setFormData] = useState({
@@ -7,7 +9,7 @@ const SignUpForm = ({ basicMode }) => {
     email: "",
     password: "",
   });
-
+  const [token, setToken] = useState("");
   const [sentVerification, setSentVerification] = useState(false);
   const [waitForNetwork, setWaitForNetwork] = useState(false);
 
@@ -17,6 +19,17 @@ const SignUpForm = ({ basicMode }) => {
     }
   }, [basicMode, waitForNetwork]);
 
+  const verifyToken = async () => {
+    try {
+      const res = axios.get(
+        `https://zarektronix-backend.onrender.com/api/user/verify/${token}`
+      );
+      console.log(res.status);
+      toast.success(res.message);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
   const postData = async () => {
     try {
       const res = await axios.post(
@@ -24,9 +37,10 @@ const SignUpForm = ({ basicMode }) => {
         formData
       );
       console.log(res, "res");
-      sentVerification(true);
+      setSentVerification(true);
     } catch (e) {
       setWaitForNetwork(false);
+      toast.error(e.response.data.error);
       console.log(e);
     }
   };
@@ -51,51 +65,78 @@ const SignUpForm = ({ basicMode }) => {
     }
   };
 
-  return !waitForNetwork ? (
-    <form
-      className="w-[500px] bg-white p-8 shadow-xl rounded-md"
-      onSubmit={handleSubmit}
-    >
-      <div className="text-xl font-bold my-5">Sign up</div>
-      <label className="block mb-4">
-        <input
-          placeholder="name"
-          className="form-input h-8 mt-1 block w-full border border-gray-300 rounded-md px-4"
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <label className="block mb-4">
-        <input
-          placeholder="email"
-          className="form-input mt-1 h-8 block w-full border border-gray-300 rounded-md px-4"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <label className="block mb-4">
-        <input
-          placeholder="password"
-          className="form-input mt-1 h-8 block w-full border border-gray-300 rounded-md px-4"
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <button className="bg-blue-500 text-white p-2 rounded-md" type="submit">
-        Submit
-      </button>
-    </form>
-  ) : (
-    <div className="">please wait while we handle the rest</div>
+  return (
+    <div className="w-[500px] bg-white p-8 shadow-xl rounded-md">
+      {!waitForNetwork && (
+        <form className="" onSubmit={handleSubmit}>
+          <div className="text-xl font-bold my-5">Sign up</div>
+          <label className="block mb-4">
+            <input
+              disabled={sentVerification}
+              placeholder="name"
+              className="form-input h-8 mt-1 block w-full border border-gray-300 rounded-md px-4"
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label className="block mb-4">
+            <input
+              disabled={sentVerification}
+              placeholder="email"
+              className="form-input mt-1 h-8 block w-full border border-gray-300 rounded-md px-4"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label className="block mb-4">
+            <input
+              disabled={sentVerification}
+              placeholder="password"
+              className="form-input mt-1 h-8 block w-full border border-gray-300 rounded-md px-4"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          {!sentVerification && (
+            <button
+              className="bg-blue-500 text-white p-2 rounded-md"
+              type="submit"
+            >
+              Submit
+            </button>
+          )}
+        </form>
+      )}
+      {waitForNetwork && <div>Please wait while we handle the rest</div>}
+      {sentVerification && !waitForNetwork && (
+        <div>
+          <input
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+            placeholder="enter verification code"
+            className="form-input h-8 block w-full border border-gray-300 rounded-md px-4 mt-4"
+            type="text"
+          />
+          <button
+            onClick={verifyToken}
+            className="bg-blue-500 text-white p-2 rounded-md mt-4"
+            type="submit"
+          >
+            verify
+          </button>
+        </div>
+      )}
+      <ToastContainer />
+    </div>
   );
 };
 
